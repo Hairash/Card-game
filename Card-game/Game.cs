b998a9f07b460588291d56169be501910aa07629
Hairash <maslevian@yandex.ru>
2020-09-04 14:13:50 +0300

    Game cycle with players #1 #2

---
+++ Card-game/Game.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Card_game
+{
+    public class CardGame
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            Console.WriteLine("App start");
+            Game game = new Game(2);
+        }
+    }
+    public class Game
+    {
+        List<Player> players;
+        int cur_player;
+
+        public Game(int num_of_players)
+        {
+            players = new List<Player>();
+            for (int i = 0; i < num_of_players; ++i)
+            {
+                Player player = new Player(i);
+                players.Add(player);
+            }
+            cur_player = 0;
+            show();
+
+            start();
+        }
+
+        void show()
+        {
+            Console.WriteLine("Game show");
+            for (int i = 0; i < players.Count; ++i)
+            {
+                players[i].show();
+            }
+        }
+
+        void start()
+        {
+            Console.WriteLine("Game");
+            int round = 0;
+            while (round < 10)
+            {
+                players[cur_player].show();
+                cur_player += 1;
+                cur_player %= 2;
+
+                ++round;
+            }
+        }
+    }
+
+    public class Player
+    {
+        int id;
+
+        public Player(int id)
+        {
+            this.id = id;
+        }
+
+        public void show()
+        {
+            Console.Write("Player: ");
+            Console.WriteLine(id);
+        }
+    }
+}
